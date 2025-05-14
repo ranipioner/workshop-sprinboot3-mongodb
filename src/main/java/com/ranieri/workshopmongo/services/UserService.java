60829fe137a11a6c67617497a202e75187b0c93c
Ranieri Pioner <rani_pioner@hotmail.com>
2025-05-14 16:56:40 -0300

    User insert with POST

--- src/main/java/com/ranieri/workshopmongo/services/UserService.java
+++ src/main/java/com/ranieri/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ranieri.workshopmongo.domain.User;
+import com.ranieri.workshopmongo.dto.UserDTO;
 import com.ranieri.workshopmongo.repository.UserRepository;
 import com.ranieri.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		 Optional<User> obj = repo.findById(id); 
 		 return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!")); 
 		} 
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
