c81b025da2c960f1337282b7153c2c27df7db585
Ranieri Pioner <rani_pioner@hotmail.com>
2025-05-15 16:50:54 -0300

    Endpoint returning the Posts from User

--- src/main/java/com/ranieri/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ranieri/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ranieri.workshopmongo.domain.Post;
 import com.ranieri.workshopmongo.domain.User;
 import com.ranieri.workshopmongo.dto.UserDTO;
 import com.ranieri.workshopmongo.services.UserService;
@@ -62,6 +63,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable  String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
