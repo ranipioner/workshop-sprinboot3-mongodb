8d58a25c80dace1e9d3de90bbe5edc7c75e7dca9
Ranieri Pioner <rani_pioner@hotmail.com>
2025-05-15 21:55:30 -0300

    Simple query with @Query

--- src/main/java/com/ranieri/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ranieri/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.ranieri.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.ranieri.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'Title' : { $regex: ?0 , $options: 'i' } }")
+	List <Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
