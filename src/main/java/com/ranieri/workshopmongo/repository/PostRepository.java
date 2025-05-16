60553864973839a55312127fec66a7c20d1c9e4d
Ranieri Pioner <rani_pioner@hotmail.com>
2025-05-15 21:49:15 -0300

    Simple query with Query Methods

--- src/main/java/com/ranieri/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ranieri/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.ranieri.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.ranieri.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
